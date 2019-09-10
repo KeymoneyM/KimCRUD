3e879255f7a3e9aa682324d10936d0aa4bed9aaa
Martin Kimani <martin.kimani@office.tangazoletu.com>
2019-09-10 11:56:33 +0300

    Servlets Configuration file

--- src/main/java/com/kim/configurations/WebAppInitializer.java
+++ src/main/java/com/kim/configurations/WebAppInitializer.java
@@ -1,4 +1,21 @@
 package com.kim.configurations;
 
-public class WebAppInitializer {
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[] {AppConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
 }
